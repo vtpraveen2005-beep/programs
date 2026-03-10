73521b923d0c02ab18708e7930e94f14e82bdcbc
PRAVEN V T <vtpraveen2005@gmail.com>
2026-03-11 04:12:06 +0530

    Update Concatenation of Array.java

--- Concatenation of Array.java
+++ Concatenation of Array.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n=nums.length;
+        int[] res=new int[2*n];
+        for(int i=0;i<n;i++){
+            res[i]=nums[i];
+            res[i+n]=nums[i];
+        }
+        return res;
+    }
+}
